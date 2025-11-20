03a12b46f7e7e9c3e16c40fae0712a60c3a94115
blex-max <ablex7@gmail.com>
2025-11-20 14:31:35 +0000

    formatting conquered, for now

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 // clang-format off
 // GET TEMPLATES AND DISTRIBUTION OF TEMPLATES SUPPORTING A VARIANT
+// for variant, report:
+// - template start total range and MAD [Median Absolute Distribution]*
+// - template size total range and MAD*
+// - leftmost template start
+// - rightmost template start
+// - leftmost template end
+// - rightmost template end
 // (optionally) merge templates within +-n bp of eachother
 // write (merged) templates with representative qnames + distribution/s
 // into VCF
+// across all variants tested, report to file:
+// - distribution of template start range
+// - distribution of template size range
+// optional reports to separate file:
+// - template details per qname
+// *If MAD ~= range, broad distribution, if range >> MAD, narrow distribution with outlier/s
+// Using MAD because it is a robust spread statistic (i.e. not sensitive to outliers)
+// and better able to deal with a low number of observations than IQR.
 // ---
 // The above in itself is probably an advancement in method for assessing variant
-// legitimacy for low yield seq. If theres very little template devation (compared to the average)
-// then almost certainly it's an artefact
+// legitimacy for low yield seq. If theres very little template variation (compared to the average)
+// then almost certainly it's an artefact. That is, you can filter on this data alone
 // ---
 // WITH BCFTOOLS CONSENSUS
 // fetch template region from reference.
@@ -16,8 +31,119 @@
 // evaluate foldiness of recovered templates
 // clang-format on
 
+#include <iostream>
 
-int main (int argc,
-          char *argv[]) {
+#include <cxxopts.hpp>
+#include <htslib/hts.h>
+#include <htslib/sam.h>
+#include <htslib/vcf.h>
+
+#include "hts_ptr_t.hpp"
+#include "pileup.hpp"
+
+// TODO allow exclusion of variants by filter flags
+// TODO allow user defined samflags for include/exclude
+// TODO support single ended?
+// TODO options to output to VCF and/or TSV (VCF to stdout)
+int main (
+    int argc,
+    char *argv[]
+) {
+    namespace fs = std::filesystem;
+    fs::path ref_path;
+    fs::path vcf_path;
+    fs::path aln_path;
+
+    cxxopts::Options options (
+        "tempex",
+        "variant fragment template extraction and statistics\n"
+    );
+
+    // clang-format off
+    options.add_options() ("h,help", "Print usage")
+        ("vcf", "VCF", cxxopts::value<fs::path>())
+        ("aln", "Sample BAM", cxxopts::value<fs::path>());
+        // ("-n,--name");  // key to match sample to VCF
+
+    options.parse_positional ({"vcf", "aln"});
+    options.positional_help ("<VCF> <ALN>");
+    options.show_positional_help();
+
+    try {
+        auto result = options.parse (argc, argv);
+
+        if (result.count ("help")) {
+            std::cout << options.help() << std::endl;
+            return 0;
+        }
+
+        if (!result.count ("vcf") || !result.count ("aln"))
+            throw std::runtime_error (
+                "All positional arguments must be provided");
+
+        vcf_path = result["vcf"].as<fs::path>();
+        aln_path = result["aln"].as<fs::path>();
+
+        if (!fs::exists (vcf_path)) {
+            throw std::runtime_error ("VCF file not found: "
+                                      + vcf_path.string());
+        }
+
+        std::cout << "Using VCF: " << vcf_path << std::endl;
+
+        if (!fs::exists (aln_path)) {
+            throw std::runtime_error ("Alignment file not found: "
+                                      + aln_path.string());
+        }
+
+        std::cout << "Using aln: " << aln_path << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << "Error parsing CLI options: " << e.what()
+                  << "\n";
+        return 1;
+    }
+    // clang-format on
+
+    auto _ain{hts_open (aln_path.c_str(), "r")};
+    if (_ain == NULL) {
+        std::cout << std::format (
+            "Could not open alignment file at {}",
+            aln_path.string()
+        ) << std::endl;
+        return 1;
+    }
+    auto _aixin{sam_index_load (_ain, aln_path.c_str())};
+    if (_aixin == NULL) {
+        std::cout << std::format (
+            "Coud not open index for alignment "
+            "file. Searched for {}.bai",
+            aln_path.c_str()
+        ) << std::endl;
+    }
+
+    auto _vin{hts_open (vcf_path.c_str(), "r")};
+    if (_vin == NULL) {
+        std::cout << std::format (
+            "Could not open VCF file at {}",
+            vcf_path.string()
+        ) << std::endl;
+        return 1;
+    }
+    htsFile_upt ap{std::move (_ain), hts_close};
+    htsFile_upt vp{std::move (_vin), hts_close};
+    hts_idx_upt apit{std::move (_aixin), hts_idx_destroy};
+
+    auto _vh{bcf_hdr_read (vp.get())};
+    bcf_hdr_upt vph{std::move (_vh), bcf_hdr_destroy};
+
+    bcf1_upt b1{bcf_init(), bcf_destroy};
+
+    template_vt var_tmpls;
+    while (bcf_read (vp.get(), vph.get(), b1.get()) == 0) {
+        // b1->errcode  // MUST CHECK BEFORE WRITE TO VCF
+        var_tmpls = get_templates (ap.get(), apit.get(), b1.get());
+        // TODO/NEXT
+    };
+
     return 0;
 }
--- src/variant.hpp
+++ src/variant.hpp
@@ -1,80 +1,90 @@
 #pragma once
 
-#include <cstdint>
 #include <cstring>
+#include <stdexcept>
+#include <string>
+
 #include <htslib/hts.h>
 #include <htslib/kstring.h>
 #include <htslib/sam.h>
-#include <string>
+#include <htslib/vcf.h>
 
 
-struct Mut {
-    int32_t rid;
-    int64_t rpos0; // 0 indexed start
-    std::string ref, alt;
-    // bool imprecise; // for imprecise indels <>
+// in caller, to assess type
+// bcf_has_variant_types (b, VCF_DEL | VCF_INS | VCF_SNP | VCF_MNP, bcf_variant_match::bcf_match_overlap)
+// preconditions:
+// -- normalised bcf1_t (single alt)
+// -- bam_pileup1_t covers variant
+inline bool evaluate_support (
+    const bam_pileup1_t *pl,
+    const bcf1_t        *b,
+    int                  mtype
+) {
 
-    size_t rlen () const noexcept { return ref.size(); }
+    // TODO (ask samteam/read source)
+    // if (!b->unpacked)
 
-    size_t qlen () const noexcept { return alt.size(); }
+    // if (b->n_allele != 1)
+    //     throw std::runtime_error ("unnormalised vcf");
 
-    int diff () const { // TODO guard lengths in constructor between 0
-                        // < x < some short length
-        // if (imprecise)
-        //     throw std::runtime_error (
-        //         "Mut: attempt to call len_diff on imprecise
-        //         indel");
-        return (rlen() > qlen()) ? -(rlen() - qlen())
-                                 : qlen() - rlen();
-    }
+    std::string ref, alt, mnv;
+    size_t      qpos;
+    char        qbase;
+    int         vdiff;
+    bool        indel_lmatch;
 
-    bool is_del () const { return (diff() < 0); }
-    bool is_ins () const { return (diff() > 0); }
-    bool is_indel () const { return (diff() != 0); }
+    switch (mtype) {
+        case (VCF_DEL):
+        case (VCF_INS):
+        case (VCF_SNP):
+        case (VCF_MNP):
+            ref = b->d.allele[0];
+            alt = b->d.allele[1];
 
-    // TODO method of Mut
-    // return 1 if bam_pileup1_t supports read
-    // 0 otherwise. -1 on error
-    bool evaluate_support (const bam_pileup1_t *pl) const {
-        auto qpos = static_cast<size_t> (pl->qpos);
+            qpos  = static_cast<size_t> (pl->qpos);
+            qbase = seq_nt16_str[bam_get_seq (pl->b)[qpos]];
+            if (qbase != alt[0]) // compare to first character of mutation
+                return false; // all mutation types must conform
+            if (VCF_SNP)      // only relevant condition for SNP
+                return true;
 
-        auto qbase (seq_nt16_str[bam_get_seq (pl->b)[qpos]]);
-        if (qbase != alt[0])
-            return false; // all mutation types must conform
+            vdiff = static_cast<int> (alt.length())
+                    - static_cast<int> (ref.length()); // negative for del, pos for ins
+            indel_lmatch = (vdiff == pl->indel);
 
-        auto indel_lmatch =
-            diff() == pl->indel; // both + for ins, - for del
-        if (is_indel() && !indel_lmatch)
-            return false;
-        if (is_del())
-            return static_cast<int> (indel_lmatch);
-
-        if (is_ins()) {
-            kstring_t ins;
-            ks_initialize (&ins);
-            auto rc = bam_plp_insertion (pl, &ins, NULL);
-            if (rc != diff()) { // failure or wrong length
-                ks_free (&ins);
+            // needs verification
+            if ((mtype & (VCF_INS | VCF_DEL)) && !indel_lmatch)
                 return false;
+            if (mtype == VCF_DEL)
+                return indel_lmatch; // i.e. true due to the previous conditional
+
+            // needs verification
+            if (mtype == VCF_INS) {
+                kstring_t ins;
+                ks_initialize (&ins);
+                auto rc = bam_plp_insertion (pl, &ins, NULL);
+                if (rc != vdiff) { // failure or wrong length
+                    ks_free (&ins);
+                    return false;
+                }
+                auto ins_match
+                    = static_cast<int> ( // check bases match
+                        strcmp (ks_c_str (&ins), alt.c_str()) == 0);
+                ks_free (&ins);
+                return (ins_match);
             }
-            auto ins_match = static_cast<int> ( // check bases match
-                strcmp (ks_c_str (&ins), alt.c_str()) == 0);
-            ks_free (&ins);
-            return (ins_match);
-        }
 
-        if (qlen() == 1) // SNV
-            return true;
+            // only mnv remains
+            mnv; // must be a MNV
+            for (size_t i = 0; i < alt.length(); ++i) {
+                mnv.push_back (seq_nt16_str[bam_get_seq (pl->b)[qpos + i]]);
+            }
 
-        std::string mnv; // must be a MNV
-        for (size_t i = 0; i < qlen(); ++i) {
-            mnv.push_back (
-                seq_nt16_str[bam_get_seq (pl->b)[qpos + i]]);
-        }
-
-        if (mnv == alt)
-            return true;
-
-        return false;
+            return (mnv == alt);
+        default:
+            throw std::runtime_error (
+                "type does not match one and only one of VCF_DEL, "
+                "VCF_INS, VCF_SNP, VCF_MNP"
+            );
     }
-};
+}
