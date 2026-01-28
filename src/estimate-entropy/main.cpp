c4318f91d55f8d9dcc1ad7be337e717dcf163342
blex-max <ablex7@gmail.com>
2026-01-28 14:20:49 +0000

    add new structure

---
+++ src/estimate-entropy/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <cxxopts.hpp>
+
+#include "lib/stats.hpp"
+
+int main (int argc, char ** argv) {
+  std::string input;
+
+  cxxopts::Options args (
+    "estimate-entropy",
+    "\n"
+    "lempel-ziv estimated entropy rate per character of input string"
+  );
+
+  args.add_options()
+    ("h,help", "Print usage")
+    ("str", "input string", cxxopts::value<std::string>());
+
+  args.parse_positional({"str"});
+  args.positional_help("<STR>");
+
+  {
+    auto parsedargs = args.parse(argc, argv);
+
+    if (parsedargs.count ("help")) {
+        std::cout << args.help() << std::endl;
+        return 1;
+    }
+
+    if (!parsedargs.count ("str")) {
+        std::cout << "All positional arguments must be provided" << std::endl;
+        return 1;
+    } else {
+      input = parsedargs["str"].as<std::string>();
+    }
+  }
+
+  std::cout << std::to_string(entropy_lz76(input)) << std::endl;
+
+  return 0;
+}
